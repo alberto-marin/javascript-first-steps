b85344d787acdca7eec498ebd89cd983078eb5a6
Alberto Marin <hello@albertomar.in>
2017-02-17 15:36:37 +0000

    Add newStory replacements

--- silly-story-generator/main.js
+++ silly-story-generator/main.js
@@ -8,19 +8,28 @@ function randomValueFromArray(array) {
 
 var storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-var insertX = "Willy the Goblin, Big Daddy, Father Christmas";
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-var insertY = "the soup kitchen, Disneyland, the White House";
+var insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
-var insertZ = "spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away";
+var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
 function result() {
+	var newStory = storyText;
+	var xItem = randomValueFromArray(insertX);
+	var yItem = randomValueFromArray(insertY);
+	var zItem = randomValueFromArray(insertZ);
+	
+	newStory = newStory.replace(':insertx:',xItem);
+	newStory = newStory.replace(':insertx:',xItem);
+	newStory = newStory.replace(':inserty:',yItem);
+	newStory = newStory.replace(':insertz:',zItem);
   
   if (customName.value != '') {
     var name = customName.value;
-    
+    newStory = newStory.replace('Bob', name);
   }
 
   if (document.getElementById("uk").checked) {
@@ -29,6 +38,6 @@ function result() {
     
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
